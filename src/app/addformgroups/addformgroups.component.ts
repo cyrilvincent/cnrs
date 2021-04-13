b21d0c049d809336fb937bd409a95240ec9d09a6
cyrilvincent <contact@cyrilvincent.com>
2021-04-13 18:37:21 +0200

    nice

--- src/app/addformgroups/addformgroups.component.ts
+++ src/app/addformgroups/addformgroups.component.ts
@@ -1,54 +1,26 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, FormArray, Validators } from "@angular/forms";
-import { MockDb } from '../db'
+import * as dbjson from '../../assets/db.json';
 
 @Component({
   selector: "app-addformgroups",
-  templateUrl: "./addformgroups.component.html"
+  templateUrl: "./addformgroups.component.html",
+  styleUrls: ['./addformgroups.component.scss']
 })
 export class AddformgroupsComponent implements OnInit {
   public form: FormGroup;
   values = {};
   nb = 0;
-  db = {} // db complet en json
-  db2: { [id: number] : any; } = {}
-
-  vm = [
-    /*{
-      value: "user 0",
-      label: "label 0",
-      key: 0,
-      level: 0,
-      type: "text",
-      parentId: -1,
-    },
-    {
-      value: "b",
-      label: "label 1",
-      key: 1,
-      level: 1,
-      parentId: 0,
-      type: "select",
-      options: [
-        {key: 'a',  value: 'A'},
-        {key: 'b',  value: 'B'},
-        {key: 'c',  value: 'C'},
-        {key: 'd', value: 'D'}
-      ],
-    }*/
-  ]
+  db: { [id: number] : any; } = {}
+  vms = []
   
   constructor(private fb: FormBuilder) {
-    this.db2 = new MockDb().db;
-    for(let key=0;key<100;key++) {
-      let level = key // simplification du pb
-      this.db[key]={id:key, level:level, parentId:key-1}
-    }
+    this.db = dbjson["default"];
   }
 
   ngOnInit() {
     let array = []
-    this.vm.forEach(
+    this.vms.forEach(
       item => array.push(this.fb.group(item))
     )
     this.form = this.fb.group({
@@ -56,11 +28,12 @@ export class AddformgroupsComponent implements OnInit {
       vms: this.fb.array(array)
     });
     this.addControl(0);
+    this.show();
   }
 
   getEntitiesByParentId(parentId: number) {
-    parent = this.db2[parentId];
-    let res = Object.values(this.db2).filter(v => v.parentId == parentId );
+    parent = this.db[parentId];
+    let res = Object.values(this.db).filter(v => v.parentId == parentId );
     return res;
   }
 
@@ -71,19 +44,26 @@ export class AddformgroupsComponent implements OnInit {
     vm["key"] = entity.id;
     vm["level"] = entity.level;
     vm["parentId"] = entity.parentId;
-    vm["type"] = "select";
-    vm["options"] = [];
-    for (let e of entities) {
-      let option = {}
-      option["key"] = e.id;
-      option["value"] = e.label;
-      vm["options"].push(option);
+    if (entities.length == 0) {
+      vm["type"] = "text";
+      vm["value"] = "";
+    }
+    else {
+      vm["type"] = "select";
+      vm["options"] = [];
+      entities.sort((a, b) => a.order - b.order);
+      for (let e of entities) {
+        let option = {}
+        option["key"] = e.id;
+        option["value"] = e.shortLabel;
+        vm["options"].push(option);
+      }
     }
     return vm;
   }
 
   getVMById(id: number) {
-    let entity = this.db2[id];
+    let entity = this.db[id];
     let entities = this.getEntitiesByParentId(entity.id);
     let vm = this.convertEntityToVM(entity, entities)
     return vm;
@@ -93,63 +73,38 @@ export class AddformgroupsComponent implements OnInit {
     console.log("Add control");
     let array = this.form.controls.vms as FormArray;
     let item = this.getVMById(id);
-    this.vm.push(item)
+    this.vms.push(item)
     let gitem: FormGroup = this.fb.group(item);
     array.push(gitem);
     this.nb += 1;
   }
 
-  addFormControl() {
-    console.log("Add level");
-    let array = this.form.controls.vms as FormArray;
-    let item = {
-      value: "g",
-      key: this.nb,
-      label: "new "+this.nb,
-      level: this.nb,
-      parentId: this.nb - 1,
-      type: "select",
-      options: [
-        {key: 'e',  value: 'E'},
-        {key: 'f',  value: 'F'},
-        {key: 'g',   value: 'G'},
-        {key: 'h', value: 'H'}
-      ],
-    }
-
-    this.nb += 1;
-    this.vm.push(item);
-    let gitem: FormGroup = this.fb.group(item);
-    array.push(gitem);
-
-  }
-  
   removeFormControl() {
     console.log("Remove last level");
     let array = this.form.controls.vms as FormArray;
-    this.vm.pop();
-    array.removeAt(array.length - 1);   
+    this.vms.pop();
+    array.removeAt(array.length - 1);
+    this.nb -= 1; 
   }
 
   show() {
     this.values = this.form.getRawValue();
-    let vms = this.getVMById(0);
-    console.log(vms);
   }
 
   change(value) {
     let id = value.value;
-    let entity = this.db2[id];
-
+    let entity = this.db[id];
     console.log("Change from "+entity.parentId+"=>"+id);
     let level = entity.level;
-    for(let i = level; i < this.nb; i++) {
+    let nb = this.nb
+    for(let i = level; i < nb; i++) {
         this.removeFormControl();
     }
-    this.nb += level - this.nb
-    //this.addFormControl();
     this.addControl(id);
     this.show();
+  }
 
+  lastChange(value) {
+    this.show();
   }
 }
