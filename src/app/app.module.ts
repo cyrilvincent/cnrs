65fb572d992bb1fbf5d9b061f764b2c7c6ec1d7c
cyrilvincent <contact@cyrilvincent.com>
2021-04-13 13:04:48 +0200

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from "./app.component";
-import { BasicComponent } from "./basic/basic.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { AddformgroupsComponent } from "./addformgroups/addformgroups.component";
 import { MaterialModule} from "./material.module";
 
 @NgModule({
-  declarations: [AppComponent, BasicComponent, AddformgroupsComponent],
+  declarations: [AppComponent, AddformgroupsComponent],
   imports: [BrowserModule, ReactiveFormsModule, MaterialModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
