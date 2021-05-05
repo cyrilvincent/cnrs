597263c54c9d09a46f8a90cbd2f9d003a380558d
cyrilvincent <contact@cyrilvincent.com>
2021-05-05 15:21:27 +0200

    xslx2json

---
+++ python/mind.js
@@ -0,0 +1,120 @@
+var dbmind = {
+ "meta": {
+  "name": "demo",
+  "author": "contact@cyrilvincent.com",
+  "version": "0.1"
+ },
+ "format": "node_array",
+ "data": [
+  {
+   "id": "root",
+   "isroot": true,
+   "topic": "1"
+  },
+  {
+   "id": "2",
+   "topic": "1.1",
+   "parentid": "1"
+  },
+  {
+   "id": "3",
+   "topic": "1.1.1",
+   "parentid": "2"
+  },
+  {
+   "id": "4",
+   "topic": "1.1.1.1",
+   "parentid": "3"
+  },
+  {
+   "id": "5",
+   "topic": "1.1.1.2",
+   "parentid": "3"
+  },
+  {
+   "id": "6",
+   "topic": "1.1.1.3",
+   "parentid": "3"
+  },
+  {
+   "id": "7",
+   "topic": "1.1.1.3.1",
+   "parentid": "6"
+  },
+  {
+   "id": "8",
+   "topic": "1.1.1.3.2",
+   "parentid": "6"
+  },
+  {
+   "id": "9",
+   "topic": "1.1.1.3.3",
+   "parentid": "6"
+  },
+  {
+   "id": "10",
+   "topic": "1.1.2",
+   "parentid": "2"
+  },
+  {
+   "id": "11",
+   "topic": "1.1.3",
+   "parentid": "2"
+  },
+  {
+   "id": "12",
+   "topic": "1.2",
+   "parentid": "1"
+  },
+  {
+   "id": "13",
+   "topic": "1.2.1",
+   "parentid": "12"
+  },
+  {
+   "id": "14",
+   "topic": "1.2.1.1",
+   "parentid": "13"
+  },
+  {
+   "id": "15",
+   "topic": "1.2.1.1.1",
+   "parentid": "14"
+  },
+  {
+   "id": "16",
+   "topic": "1.2.1.1.2",
+   "parentid": "14"
+  },
+  {
+   "id": "17",
+   "topic": "1.3",
+   "parentid": "1"
+  },
+  {
+   "id": "18",
+   "topic": "1.3.1",
+   "parentid": "17"
+  },
+  {
+   "id": "19",
+   "topic": "1.4",
+   "parentid": "1"
+  },
+  {
+   "id": "20",
+   "topic": "1.4.1",
+   "parentid": "19"
+  },
+  {
+   "id": "21",
+   "topic": "1.5",
+   "parentid": "1"
+  },
+  {
+   "id": "22",
+   "topic": "1.5.1",
+   "parentid": "21"
+  }
+ ]
+}
--- python/xlsx2json.py
+++ python/xlsx2json.py
@@ -1,49 +1,36 @@
 import json
+import openpyxl
 
-class MindMeisterRtfParser:
+
+class XlEquipmentParser:
 
     def __init__(self):
         self.rownum = 0
         self.db = []
         self.entities = []
+        self.minds = []
+        self.id = 1
+        self.keys = set()
 
     def load(self, path):
-        with open(path) as f:
-            id = 0
-            while True:
-                row = self.next(f)
-                if row is None:
-                    break
-                if row.startswith(r"\slmult0\ltrpar\li"):
-                    index = row.rindex(r"\li")
-                    s = row[index+3:]
-                    level = int(int(s) / 200)
-                    self.next(f)
-                    row = self.next(f)
-                    row = self.unicode(row)
-                    self.db.append((id, level, row))
-                    id += 1
+        print(f"Open {path}")
+        wb = openpyxl.load_workbook(path)
+        for sheet in wb.worksheets:
+            self.parse_sheet(sheet)
+        print(self.db)
+        wb.close()
 
-    def next(self, f):
-        row = f.readline()
-        self.rownum += 1
-        if row == "":
-            return None
-        return row.strip()
-
-    def unicode(self, s):
-        s = s.replace(r"\'3f", "")
-        s = s.replace(r"\u201", "E")
-        s = s.replace(r"\u224", "à")
-        s = s.replace(r"\u232", "è")
-        s = s.replace(r"\u233", "é")
-        s = s.replace(r"\u234", "ê")
-        s = s.replace(r"\u244", "ô")
-        s = s.replace(r"\u246", "ö")
-        s = s.replace(r"\u947", "γ")
-        s = s.replace(r"\u8211", "-")
-        s = s.replace(r"\u8217", "'")
-        return s
+    def parse_sheet(self, sheet):
+        print(f"Opened sheet {sheet.title} with {sheet.max_row} rows and {sheet.max_column} columns")
+        for i in range(1, sheet.max_row + 1):
+            for j in range(1, sheet.max_column + 1):
+                v = sheet.cell(i, j).value
+                if v is not None:
+                    v = str(v).strip()
+                    if (j, v) not in self.keys:
+                        self.keys.add((j, v))
+                        self.db.append((self.id, j - 1, v))
+                        self.id += 1
 
     def convert(self):
         nblevel = max([item[1] for item in self.db]) + 1
@@ -59,7 +46,7 @@ class MindMeisterRtfParser:
             pid = ids[level - 1] if level > 0 else -1
             label = short if level <= 2 else f"{[e for e in self.entities if e['id'] == pid][0]['label']}, {short}"
             ids[level] = id
-            if level <= prevlevel and prevlevel != -1:
+            if level <= prevlevel != -1:
                 self.entities[-1]["leaf"] = True
             entity = self.entity(id, label, short, pid, order)
             self.entities.append(entity)
@@ -83,13 +70,65 @@ class MindMeisterRtfParser:
         with open(path, "w") as f:
             f.write(js)
 
+    def convert_mind(self):
+        nblevel = max([item[1] for item in self.db]) + 1
+        ids = []
+        for i in range(nblevel):
+            ids.append(-1)
+        even = True
+        for item in self.db:
+            id = item[0]
+            level = item[1]
+            short = item[2]
+            pid = ids[level - 1] if level > 0 else -1
+            ids[level] = id
+            entity = self.entity_mind(id, short, pid)
+            if pid == 0:
+                if even:
+                    entity["direction"] = "right"
+                else:
+                    entity["direction"] = "left"
+                even = not even
+            self.minds.append(entity)
+
+    def entity_mind(self, id, topic, parentid):
+        if parentid == -1:
+            return {
+                "id": "root",
+                "isroot": True,
+                "topic": topic,
+            }
+        else:
+            return {
+                "id": str(id),
+                "topic": topic,
+                "parentid": "root" if parentid == 0 else str(parentid),
+            }
+
+    def save_mind(self, path):
+        dict = {"meta":
+                {
+                    "name": "demo",
+                    "author": "contact@cyrilvincent.com",
+                    "version": "0.1",
+                },
+                "format": "node_array",
+                "data": self.minds}
+        js = json.dumps(dict, indent=True)
+        with open(path, "w") as f:
+            f.write("var dbmind = ")
+            f.write(js)
+
 
 if __name__ == '__main__':
-    print("RTF2JSON")
-    print("========")
-    path = "../src/assets/Spectroscopie.rtf"
-    p = MindMeisterRtfParser()
+    print("XSLX2JSON")
+    print("=========")
+    path = "eq.xlsx"
+    p = XlEquipmentParser()
     p.load(path)
     p.convert()
-    path = path.replace("Spectroscopie.rtf", "db.json")
+    path = path.replace("eq.xlsx", "db.json")
     p.save(path)
+    p.convert_mind()
+    path = path.replace("db.json", "mind.js")
+    p.save_mind(path)
